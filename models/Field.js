c40fba9d53051248084818df32440799d6634a92
Anderson Dantas <a.kleber.d@gmail.com>
2019-11-04 13:21:33 -0300

    Simplefied validation on gps point

--- models/Field.js
+++ models/Field.js
@@ -10,22 +10,14 @@ const FieldSchema = {
         type: Sequelize.FLOAT,
         alloweNull: false,
         validate: {
-            isLatitude(value) {
-                if (!(-90 <= value && value <= 90)) {
-                    throw new Error('Latitude should be in [-90, 90]');
-                }
-            }
+            len: [-90, 90]
         }
     },
     longitude: {
         type: Sequelize.FLOAT,
         alloweNull: false,
         validate: {
-            isLongitude(value) {
-                if (!(-180 <= value && value <= 180)) {
-                    throw new Error('Longitude should be in [-180, 180]');
-                }
-            }
+            len: [-180, 180]
         }
     },
 }
